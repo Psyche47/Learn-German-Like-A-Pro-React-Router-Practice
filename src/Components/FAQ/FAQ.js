b263a11a9a1f72196a12be851855473bbacb9100
Psyche47 <emmanuel.gomes7181@gmail.com>
2021-10-04 23:56:42 +0600

    FAQ updated

--- src/Components/FAQ/FAQ.js
+++ src/Components/FAQ/FAQ.js
@@ -9,23 +9,23 @@ const FAQ = () => {
       </h2>
       <ListGroup>
         <ListGroup.Item>1. Who can take these courses?</ListGroup.Item>
-        <ListGroup.Item>
+        <ListGroup.Item variant="success">
           Ans: Our courses are online at the moment and everyone can enroll.
         </ListGroup.Item>
         <ListGroup.Item>2. How to pay fees?</ListGroup.Item>
-        <ListGroup.Item>
+        <ListGroup.Item variant="success">
           Ans: A lot of gateways are available. Preferred is Paypal.
         </ListGroup.Item>
         <ListGroup.Item>3. How many lessons are in the courses?</ListGroup.Item>
-        <ListGroup.Item>
+        <ListGroup.Item variant="success">
           Ans: Total of 32 lessons in Intensive and 72 in Extensive.
         </ListGroup.Item>
         <ListGroup.Item>4. Are the classes live or recorded?</ListGroup.Item>
-        <ListGroup.Item>
+        <ListGroup.Item variant="success">
           Ans: Classes are a mixture of live and recorded.
         </ListGroup.Item>
         <ListGroup.Item>5. Will books be provided?</ListGroup.Item>
-        <ListGroup.Item>
+        <ListGroup.Item variant="success">
           Ans: 2 Ebooks and a lot of exercise PDF's will be given.
         </ListGroup.Item>
       </ListGroup>
