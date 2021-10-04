dcb6278d2939a24cb9d8616f5bf7a47e92738166
Psyche47 <emmanuel.gomes7181@gmail.com>
2021-10-04 23:09:26 +0600

    Services page added

--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
-import "./Service.css";
 
 const Service = (props) => {
   const { title, type, schedule, total_duration, fee, img } = props.course;
@@ -13,13 +12,13 @@ const Service = (props) => {
       <Card style={{ width: "18rem" }} className="mt-3">
         <Card.Img variant="top" src={img} style={cardImg} />
         <Card.Body>
-          <Card.Title>{title}</Card.Title>
+          <Card.Title>Course Name: {title}</Card.Title>
         </Card.Body>
         <ListGroup className="list-group-flush">
-          <ListGroupItem>{type}</ListGroupItem>
-          <ListGroupItem>{total_duration}</ListGroupItem>
-          <ListGroupItem>{schedule}</ListGroupItem>
-          <ListGroupItem>{fee}</ListGroupItem>
+          <ListGroupItem>Course Type: {type}</ListGroupItem>
+          <ListGroupItem>Total Duration: {total_duration}</ListGroupItem>
+          <ListGroupItem>Schedule: {schedule}</ListGroupItem>
+          <ListGroupItem>Fee: ${fee}</ListGroupItem>
         </ListGroup>
       </Card>
     </div>
--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import Service from "../Service/Service";
 
 const Services = () => {
+  const [courses, setCourses] = useState([]);
+  useEffect(() => {
+    fetch("./services.json")
+      .then((res) => res.json())
+      .then((data) => setCourses(data));
+  }, []);
   return (
-    <div>
-      <h2>Offered Services</h2>
+    <div className="container">
+      <h3 className="rounded bg-primary text-white text-center mt-3 p-3">
+        All of our specialized courses
+      </h3>
+      <div className="row">
+        {courses.map((course) => (
+          <Service key={course.course_id} course={course}></Service>
+        ))}
+      </div>
     </div>
   );
 };
