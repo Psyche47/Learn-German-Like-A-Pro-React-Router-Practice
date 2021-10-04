8ca262c6f32389ae4e915773c37170f99ddd00e9
Psyche47 <emmanuel.gomes7181@gmail.com>
2021-10-04 23:01:15 +0600

    Home page fixed

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,43 @@
 import React from "react";
-
+import { useEffect } from "react";
+import { useState } from "react";
+import { Container } from "react-bootstrap";
+import header_img from "../../Images/header.jpg";
+import Service from "../Service/Service";
 const Home = () => {
+  const [courses, setCourses] = useState([]);
+  useEffect(() => {
+    fetch("./services.json")
+      .then((res) => res.json())
+      .then((data) => setCourses(data));
+  }, []);
   return (
     <div>
-      <h2>Home</h2>
+      <section>
+        <div className="container d-flex">
+          <div>
+            <img src={header_img} alt="" />
+          </div>
+          <div className="ms-3">
+            <h4>Learn From Our Expert Teachers.</h4>
+            <h4>Ace your German Exam.</h4>
+          </div>
+        </div>
+      </section>
+      <section>
+        <Container>
+          <h3 className="text-center bg-dark text-white p-3 mt-2">
+            Some of our most popular courses
+          </h3>
+        </Container>
+        <Container>
+          <div className="row">
+            {courses.slice(0, 5).map((course) => (
+              <Service key={course.course_id} course={course}></Service>
+            ))}
+          </div>
+        </Container>
+      </section>
     </div>
   );
 };
--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
+import "./Service.css";
 
-const Service = () => {
+const Service = (props) => {
+  const { title, type, schedule, total_duration, fee, img } = props.course;
+  const cardImg = {
+    height: "143px",
+    width: "286px",
+  };
   return (
-    <div>
-      <h2>Indivisual Services</h2>
+    <div className="col-lg-3 col-md-3">
+      <Card style={{ width: "18rem" }} className="mt-3">
+        <Card.Img variant="top" src={img} style={cardImg} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          <ListGroupItem>{type}</ListGroupItem>
+          <ListGroupItem>{total_duration}</ListGroupItem>
+          <ListGroupItem>{schedule}</ListGroupItem>
+          <ListGroupItem>{fee}</ListGroupItem>
+        </ListGroup>
+      </Card>
     </div>
   );
 };
