a9954f771199bb8f3563da93b50e8d7f8c734d4e
Psyche47 <emmanuel.gomes7181@gmail.com>
2021-10-05 23:18:19 +0600

    Finished Footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import { Facebook } from "react-bootstrap-icons";
+import { Facebook, Twitter, Youtube } from "react-bootstrap-icons";
 import { NavLink } from "react-router-dom";
 import flagImg from "../../Images/germany.png";
 import "./Footer.css";
@@ -15,7 +15,7 @@ const Footer = () => {
               <img src={flagImg} alt="" className="flag-img" />
               <h5 className="brand ms-2">Learn German Like A Pro</h5>
             </Col>
-            <Col>
+            <Col className="ms-4">
               <h5 className="link-heading">Useful Links</h5>
               <div className="link-container">
                 <NavLink to="/services" className="active">
@@ -31,8 +31,56 @@ const Footer = () => {
             </Col>
             <Col>
               <h5 className="link-heading">Contact Us</h5>
-              <Facebook size={30}></Facebook>
-              <a href=""></a>
+              <div className="d-flex flex-column">
+                <div>
+                  <Facebook size={30}></Facebook>
+                  <a
+                    href="https://www.facebook.com/learn.german.language/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="active-social"
+                  >
+                    Facebook
+                  </a>
+                </div>
+                <div className="mt-2">
+                  <Youtube size={30}></Youtube>
+                  <a
+                    href="https://www.youtube.com/yourgermanteacher"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="active-social"
+                  >
+                    Youtube
+                  </a>
+                </div>
+                <div className="mt-2">
+                  <Twitter size={30}></Twitter>
+                  <a
+                    href="https://twitter.com/learngerman"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="active-social"
+                  >
+                    Twitter
+                  </a>
+                </div>
+              </div>
+            </Col>
+            <Col>
+              <h5 className="link-heading">Subscribe to our Newsletter</h5>
+              <div class="input-group mb-3">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Your Email"
+                  aria-label="Recipient's username"
+                  aria-describedby="basic-addon2"
+                />
+                <button type="button" class="btn btn-secondary">
+                  Subscribe
+                </button>
+              </div>
             </Col>
           </Row>
         </Container>
